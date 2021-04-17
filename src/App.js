c7fc1183519178a417f849c7a9e218635c435d53
majasveljo <majasveljo@gmail.com>
2021-04-17 11:13:34 +0200

    Wrapped App in strict mode

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt me!</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cocktiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1>Adopt me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
