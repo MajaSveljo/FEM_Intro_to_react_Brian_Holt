8e3f3bc9437bd9c4264a1e1960da8487d6454838
majasveljo <majasveljo@gmail.com>
2021-04-18 12:34:09 +0200

    Implemented Context theme chaning in SearchParams. Replaced a with Link in Pet in order for previous to work

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -11,7 +11,7 @@ const SearchParams = () => {
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   const [pets, setPets] = useState([]);
   // theme context can be something else and not only an array
-  const [theme] = useContext(ThemeContext);
+  const [theme, setTheme] = useContext(ThemeContext);
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -53,6 +53,21 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
+        {/* not using already existing dropdown because we need Apps hook
+        and Dropdown creates its own hook */}
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="chartreuse">Chartreuse</option>
+          </select>
+        </label>
         <button style={{ backgroundColor: theme }}>Submit</button>
       </form>
       <Results pets={pets} />
